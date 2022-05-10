84c9aef66ddbc5eae539f792a78ecdc46407b0ec
Zachary <me@dusterthefirst.com>
2022-05-10 20:32:35 +0200

    Simplified serial port handling

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,28 @@
 use std::path::PathBuf;
 
+use argh::FromArgs;
 use eframe::{
     egui::{containers::ComboBox, CentralPanel, Context, DragValue, TopBottomPanel},
     App, Frame, NativeOptions,
 };
-use serial2::SerialPort;
 use serial_worker::SerialWorker;
-use argh::FromArgs;
 
 mod serial_worker;
 
 /// Visualization tool for the DBL Venus Exploration project
-#[derive(FromArgs)]
+#[derive(FromArgs, Debug)]
 struct Args {
     /// serial port to connect to on startup
     #[argh(positional)]
-    port: Option<PathBuf>,
+    port: String,
+
     /// default baud rate to use
     #[argh(option)]
     baud: Option<u32>,
+
+    /// list the available ports
+    #[argh(switch)]
+    list: bool,
 }
 
 fn main() -> color_eyre::Result<()> {
@@ -27,35 +31,34 @@ fn main() -> color_eyre::Result<()> {
 
     let args: Args = argh::from_env();
 
+    dbg!(&args);
+
+    // serialport::available_ports();
+
     eframe::run_native(
         env!("CARGO_PKG_NAME"),
         NativeOptions {
             ..Default::default()
         },
         Box::new(move |ctx| {
-            let mut serial = SerialWorker::spawn(Box::new({
-                let ctx = ctx.egui_ctx.clone();
-                move || ctx.request_repaint()
-            }));
-
-            let default_baud = args.baud.unwrap_or(9600);
-
-            if let Some(port) = args.port {
-                serial.connect(port, default_baud);
-            }
+            let baud = args.baud.unwrap_or(9600);
 
             Box::new(Application {
-                serial_ports: SerialPort::available_ports().unwrap(),
-                baud_rate: default_baud,
-                serial,
+                serial: SerialWorker::spawn(
+                    serialport::new(args.port, baud)
+                        .open()
+                        .expect("failed to open serial port"),
+                    Box::new({
+                        let ctx = ctx.egui_ctx.clone();
+                        move || ctx.request_repaint()
+                    }),
+                ),
             })
         }),
     )
 }
 
 struct Application {
-    serial_ports: Vec<PathBuf>,
-    baud_rate: u32,
     serial: SerialWorker,
 }
 
@@ -63,38 +66,10 @@ impl App for Application {
     fn update(&mut self, ctx: &Context, _frame: &mut Frame) {
         TopBottomPanel::top("serial_select").show(ctx, |ui| {
             ui.horizontal(|ui| {
-                ui.add(DragValue::new(&mut self.baud_rate).suffix(" baud"));
-
-                if ui.button("Refresh").clicked() {
-                    // TODO: do this not in ui thread maybe?
-                    self.serial_ports = SerialPort::available_ports().unwrap();
-                }
-
-                ComboBox::new("serial_port_selector", "UART port")
-                    .selected_text(
-                        self.serial
-                            .connected_port()
-                            .map(|selected_port| selected_port.to_string_lossy().into_owned())
-                            .unwrap_or_else(|| "none".into()),
-                    )
-                    .show_ui(ui, |ui| {
-                        for port in &self.serial_ports {
-                            if ui
-                                .selectable_label(
-                                    self.serial.connected_port() == Some(port),
-                                    port.to_string_lossy().into_owned(),
-                                )
-                                .clicked()
-                            {
-                                self.serial.connect(port.clone(), self.baud_rate);
-                            }
-                        }
-                    });
+                ui.label(self.serial.connected().to_string());
             });
         });
 
-        CentralPanel::default().show(ctx, |ui| {
-
-        });
+        CentralPanel::default().show(ctx, |ui| {});
     }
 }
--- src/serial_worker.rs
+++ src/serial_worker.rs
@@ -9,63 +9,46 @@ use std::{
     thread::{self, JoinHandle},
 };
 
-use serial2::SerialPort;
+use serialport::SerialPort;
 use tracing::{error, info, warn};
 
 pub struct SerialWorker {
-    port_path: Option<PathBuf>,
     connected: Arc<AtomicBool>,
 
-    command_tx: Sender<SerialWorkerCommand>,
     packet_rx: Receiver<SerialPacket>,
 
     handle: JoinHandle<()>,
 }
 
-enum SerialWorkerCommand {
-    Connect { name: PathBuf, baud: u32 },
-    Disconnect,
-}
-
 impl SerialWorker {
-    pub fn spawn(repaint: Box<impl Fn() + Send + 'static>) -> SerialWorker {
-        let (command_tx, command_rx) = channel();
+    pub fn spawn(
+        serial_port: Box<dyn SerialPort>,
+        repaint: Box<impl Fn() + Send + 'static>,
+    ) -> SerialWorker {
         let (packet_tx, packet_rx) = channel();
 
-        let connected = Arc::new(AtomicBool::new(false));
+        let connected = Arc::new(AtomicBool::new(true));
 
         let handle = thread::Builder::new()
             .name("serial_worker".into())
             .spawn({
                 let connected = connected.clone();
 
-                move || serial_worker(command_rx, packet_tx, connected, repaint)
+                move || serial_worker(serial_port, packet_tx, connected, repaint)
             })
             .expect("failed to spawn serial worker thread");
 
         Self {
-            command_tx,
             packet_rx,
 
             handle,
 
-            port_path: None,
             connected,
         }
     }
 
-    pub fn connected_port(&self) -> Option<&PathBuf> {
-        self.connected
-            .load(Ordering::SeqCst)
-            .then(|| self.port_path.as_ref())
-            .flatten()
-    }
-
-    pub fn connect(&mut self, port: PathBuf, baud: u32) {
-        self.port_path = Some(port.clone());
-        self.command_tx
-            .send(SerialWorkerCommand::Connect { name: port, baud })
-            .expect("serial worker thread has exited");
+    pub fn connected(&self) -> bool {
+        self.connected.load(Ordering::SeqCst)
     }
 }
 
@@ -75,65 +58,43 @@ pub struct SerialPacket {
 }
 
 fn serial_worker(
-    command_rx: Receiver<SerialWorkerCommand>,
+    serial_port: Box<dyn SerialPort>,
     packet_tx: Sender<SerialPacket>,
     connected: Arc<AtomicBool>,
     repaint: Box<impl Fn()>,
 ) -> ! {
-    let mut serial_port = None;
+    let mut serial_port = BufReader::new(serial_port);
     let mut packet_buffer = Vec::new();
 
     loop {
-        match command_rx.try_recv() {
-            Err(TryRecvError::Disconnected) => panic!("ui thread has existed"),
-            Err(TryRecvError::Empty) => {}
-            Ok(SerialWorkerCommand::Connect { name, baud }) => {
-                info!(?name, "Connecting to serial port");
+        match read_cobs(&mut packet_buffer, &mut serial_port) {
+            Some([0xDE, 0xAD, 0xBE, 0xEF]) => {}
+            Some(data) => {
+                warn!(
+                    ?data,
+                    "Expected header packet, got something else. Sender de-sync?"
+                );
 
-                match SerialPort::open(&name, baud) {
-                    Ok(port) => {
-                        serial_port = Some(BufReader::new(port));
-                        info!(?name, "Connected to serial port");
-                        connected.store(true, Ordering::SeqCst);
-                    }
-                    Err(e) => error!(?e, ?name, "failed to open serial port"),
-                }
+                continue;
             }
-            Ok(SerialWorkerCommand::Disconnect) => {
-                serial_port.take();
-            }
+            None => continue,
         }
 
-        if let Some(serial_port) = &mut serial_port {
-            match read_cobs(&mut packet_buffer, serial_port) {
-                Some([0xDE, 0xAD, 0xBE, 0xEF]) => {}
-                Some(data) => {
-                    warn!(
-                        ?data,
-                        "Expected header packet, got something else. Sender de-sync?"
-                    );
-
-                    continue;
-                }
-                None => continue,
+        let name = match read_cobs(&mut packet_buffer, &mut serial_port) {
+            Some(bytes) => String::from_utf8_lossy(bytes).into_owned(),
+            None => {
+                continue;
             }
+        };
 
-            let name = match read_cobs(&mut packet_buffer, serial_port) {
-                Some(bytes) => String::from_utf8_lossy(bytes).into_owned(),
-                None => {
-                    continue;
-                }
-            };
+        let data = match read_cobs(&mut packet_buffer, &mut serial_port) {
+            Some(bytes) => bytes,
+            None => {
+                continue;
+            }
+        };
 
-            let data = match read_cobs(&mut packet_buffer, serial_port) {
-                Some(bytes) => bytes,
-                None => {
-                    continue;
-                }
-            };
-
-            info!(?name, ?data, "Received packet");
-        }
+        info!(?name, ?data, "Received packet");
     }
 }
 
